a68a8f3881d5b97a2555ab4f13c698e29c00ba80
juanpavergara <vergaravillarraga@gmail.com>
2016-10-15 14:37:05 -0500

    Se adiciona test que muestra estructura fija de un Applicative con |@|

--- src/test/scala/scalaz_school/ApplicativeSchoolTest.scala
+++ src/test/scala/scalaz_school/ApplicativeSchoolTest.scala
@@ -6,7 +6,7 @@ import scalaz.syntax.ApplicativeBuilder
 
 class ApplicativeSchoolTest extends FunSuite{
 
-  test("Un Applicative Functor debe funcionar como un Functor con ap con funcion de aridad 1"){
+  test("[TEORICO] Un Applicative Functor debe funcionar como un Functor con ap con funcion de aridad 1"){
 
 //    Este case class es un type constructor.
 //    Esto quiere decir que no es un tipo per se sino que cuando se construya un MyContainer
@@ -69,7 +69,7 @@ class ApplicativeSchoolTest extends FunSuite{
 
   }
 
-  test("Un Applicative Functor debe poder aplicar funciones de aridad n>1"){
+  test("[TEORICO] Un Applicative Functor debe poder aplicar funciones de aridad n>1"){
 
     case class MyGADT[A](a:A)
 
@@ -105,30 +105,104 @@ class ApplicativeSchoolTest extends FunSuite{
 
   }
 
-  test("Un Applicative Functor se debe poder construir con scream |@|"){
+  test("La estructura de computo de un Applicative Functor debe ser fija"){
+//    Estos estados mutables serviran solo para verificar que los computos en un Applicative se ejecutan siempre
+//    y que en un Monad se ejecutan hasta el caso de falla de algun computo.
+//    Cada que se ejecute un computo se incrementaran estas variables dando cuenta que se ha ejecutado la fn.
+    var stateApplicative = 0
+    var stateMonad = 0
 
-    case class MyGADT[A](a:A)
-
-    implicit val myGADTApplicative = new Applicative[MyGADT]{
-      def point[A](a: => A): MyGADT[A] = MyGADT(a)
-      def ap[A,B](fa: => MyGADT[A])(f: => MyGADT[A => B]): MyGADT[B] = MyGADT(f.a(fa.a))
+//    Una funcion normal que evalua a Option.
+//    Fijese que si se evalua se incrementan los estados de Monad y Applciative en 1
+    def o1(i:Int) = {
+      stateApplicative = stateApplicative + 1
+      stateMonad = stateMonad + 1
+      Option(i)
     }
 
-    val Appl = Applicative[MyGADT]
+    //    Una funcion normal que evalua a Option.
+    //    Fijese que si se evalua se incrementan los estados de Monad y Applciative en 1
+    def o2(i:Int) = {
+      stateApplicative = stateApplicative + 1
+      stateMonad = stateMonad + 1
+      None
+    }
 
-    val f = (one:String) => ((two:Int) => one + two)
+//    Un GADT normal que estructura 3 Int
+    case class MyGADTInts(a:Int, b:Int, c:Int)
 
-    val a = MyGADT(1)
-    val b = MyGADT("A")
-    val f1 = MyGADT(f)
-    val f2 = Appl.point(f)
+//    Aqui esta la magia. Se ejecutan tres computos aplicativos.
+//    Cuando se dice que la estructura de un aplicativo es fija quiere decir que todos los computos se ejecutaran
+//    sin importar los resultados de cada uno. A diferencia de una monada (o un monadic applicative functor) los
+//    applicative (o naperian applicative functor) siempre ejecutan TODOS sus pasos.
+//    El uso de scream (operador |@|) toma los resultados de cada computo y los utiliza para ejecutar la funcion currificada.
+//    La funcion currificada en este caso simplemente es la construccion de un MyGADTInts
+//    Es importante ver que |@| se pudo aplicar sobre Option ¿Por qué?
+//    Porque scalaz (cats tbn lo hace) ofrece conversiones implícitcas de Option a Applicative.
+//    Sería interesante revisar si esto es posible también con cualquier otro F[]
+    val resApplicative = ( o1(1) |@|
+                           o2(2) |@|
+                           o1(3))(MyGADTInts.apply)
 
-    case class MyBusinessCaseClass(i:Int, s:String)
-    //TODO: explicar la utilidad de |@| comparandola con flatmap
-    val res: MyGADT[MyBusinessCaseClass] = (a |@| b) (MyBusinessCaseClass.apply)
+//    Lo primero que debemos revisar es que todos los computos se hayan ejecutado, tal como lo promete
+//    la estructura (o patron) Applicative
+    assert(stateApplicative == 3)
 
-    res.map(g => assert(g.i == 1))
+//    Mire como, asi hayan sido ejecutados todos los computos, el resultado de la construccion del nuevo Applicative
+//    evalua a None (porque o2(2) evalua a None)
+    assert(resApplicative == None)
+
+    stateMonad = 0
+//    Ahora hagamos exactamente lo mismo pero con Option como Monad (no como Applicative).
+//    Esto lo logramos con flatmap (ayudados con for-comp)
+    val resMonad = for{
+      a <- o1(1)
+      b <- o2(2)
+      c <- o1(3)
+    } yield MyGADTInts(a,b,c)
+
+//    Al igual que con Applicative, el resultado es None
+    assert(resMonad == None)
+//    Pero a diferencia de Applicative, con Monad solo se ejecutaron los computos hasta que se materializó
+//    una tendencia negativa de la mónada que para el caso de Option es None.
+    assert(stateMonad == 2)
+
+
 
   }
-  
+
+
+  test("Un Applicative debe permitir la aplicacion de funciones de aridad n > 1") {
+    assert(true)
+  }
+
+  test("Un Applicative debe permitir traverse"){
+
+    def f1(i:Int) = "1"
+    def f2(i:Int) = "2"
+    def f3(i:Int) = "3"
+
+    def o1(i:Int) = {
+      println(s"Ejecutado o1 con $i")
+      Option(i)
+    }
+
+    def o2(i:Int) = {
+      println(s"Ejecutado o2 con $i")
+      None
+    }
+
+    case class MyGADT(a:String, b:String)
+
+    val result: Option[List[Int]] = List(1, 2, 3) traverse { x => (x > 0) option (x + 1) }
+
+    //TODO: Documentar que traverase siempre debe evaluar a G[] donde G es el type constructor que envuelve el Appl
+    val result2 = Option(1) traverse { x => (x > 0) option (x + 1) }
+
+    println(result)
+    println(result2)
+
+    assert(true)
+
+  }
 }
